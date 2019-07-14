7efa94d93f998cf02d153f451201e57f001a9c67
samueltung <tung54@gmail.co>
2019-07-13 18:04:44 -0700

    Update models

--- Insta/models.py
+++ Insta/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts', 
+        format = 'JPEG',
+        options = {'quality': 100},
+        blank = True,
+        null = True, 
+    )
+
+    def __str__(self):
+        return self.title
